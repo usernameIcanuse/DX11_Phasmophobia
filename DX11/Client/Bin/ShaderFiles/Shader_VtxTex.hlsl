53b4a846abdfda839679797b90f7b2b13d9a29e1
usernameIcanuse <90302406+usernameIcanuse@users.noreply.github.com>
2022-08-08 19:08:44 +0900

    복구

---
+++ DX11/Client/Bin/ShaderFiles/Shader_VtxTex.hlsl
@@ -0,0 +1,90 @@
+
+matrix	g_WorldMatrix, g_ViewMatrix, g_ProjMatrix;
+
+texture2D	g_DiffuseTexture;
+
+sampler DefaultSampler = sampler_state 
+{		
+	filter = min_mag_mip_linear;
+	AddressU = wrap;
+	AddressV = wrap;
+};
+
+
+
+struct VS_IN
+{
+	float3		vPosition : POSITION;
+	float2		vTexUV : TEXCOORD0;
+};
+
+struct VS_OUT
+{
+	float4		vPosition : SV_POSITION;
+	float2		vTexUV : TEXCOORD0;
+};
+
+VS_OUT VS_MAIN(VS_IN In)
+{
+	VS_OUT		Out = (VS_OUT)0;
+
+	matrix			matWV, matWVP;
+
+	matWV = mul(g_WorldMatrix, g_ViewMatrix);
+	matWVP = mul(matWV, g_ProjMatrix);
+
+	Out.vPosition = mul(vector(In.vPosition, 1.f), matWVP);
+	Out.vTexUV = In.vTexUV;
+
+	return Out;	
+}
+
+// w나누기연산을 수행하낟. (In 투영스페이스)
+// 뷰포트 변환. (In 뷰포트(윈도우좌표))
+
+// 래스터라이즈(픽셀정볼르 생성한다. )
+
+struct PS_IN
+{
+	float4		vPosition : SV_POSITION;
+	float2		vTexUV : TEXCOORD0;
+};
+
+struct PS_OUT
+{	
+	vector		vColor : SV_TARGET0;	
+};
+
+PS_OUT PS_MAIN(PS_IN In)
+{
+	PS_OUT		Out = (PS_OUT)0;
+
+	Out.vColor = g_DiffuseTexture.Sample(DefaultSampler, In.vTexUV);
+	if (Out.vColor.a < 0.5f)
+		discard;
+
+	return Out;	
+}
+
+technique11 DefaultTechnique
+{
+	pass Default
+	{	
+		VertexShader = compile vs_5_0 VS_MAIN();
+		GeometryShader = NULL;
+		PixelShader = compile ps_5_0 PS_MAIN();
+
+	}
+	/*pass Default
+	{
+		VertexShader = compile vs_5_0 VS_MAIN();
+		GeometryShader = NULL;
+		PixelShader = compile ps_5_0 PS_MAIN();
+	}
+	pass Default
+	{
+		VertexShader = compile vs_5_0 VS_MAIN();
+		GeometryShader = NULL;
+		PixelShader = compile ps_5_0 PS_MAIN();
+	}*/
+}
